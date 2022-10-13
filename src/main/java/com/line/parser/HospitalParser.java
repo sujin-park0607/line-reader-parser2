96214f3e6f4748c17d62f31848adecf08ed5c10a
sujin0607 <goweg0607@gmail.com>
2022-10-13 13:15:27 +0900

    add isParsing test module

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,14 +3,19 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital>{
+    private String replaceAllQuot(String str){
+        return str.replaceAll("\"","");
+    }
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
         String[] districtList = splitted[1].split(" ");
 
-        String district = districtList[0] + " "+ districtList[1] + "\"";
+        String district = districtList[0] + " "+ districtList[1];
         String address = splitted[1].replace("\'","\\'");
         int emergencyRoom = Integer.parseInt(splitted[6].replace("\"",""));
-        return new Hospital(splitted[0], address, district, splitted[2], emergencyRoom, splitted[10], null);
+        return new Hospital(replaceAllQuot(splitted[0]), replaceAllQuot(address),
+                            replaceAllQuot(district), replaceAllQuot(splitted[2]),
+                            emergencyRoom, replaceAllQuot(splitted[10]), null);
     }
 }
