a5fc56e478d4db0354ae747d586e0f4c91df59d6
sujin0607 <goweg0607@gmail.com>
2022-10-17 13:35:10 +0900

    자바 데이터베이스 연동 insert, select

--- src/main/java/com/line/database/SQLtest.java
+++ src/main/java/com/line/database/SQLtest.java
@@ -4,26 +4,47 @@ import java.sql.*;
 import java.util.Map;
 
 public class SQLtest {
-    public void add() throws SQLException {
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    public void connect() throws SQLException {
         Map<String, String> env = System.getenv();
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
         String dbPassword = env.get("DB_PASSWORD");
 
-        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO seoul_hospital(id, name, password) VALUES (?,?,?)");
+        conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+    }
+    public void add() throws SQLException {
+
+        ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?,?,?)");
         ps.setString(1,"1");
-        ps.setString(1,"Sujin");
-        ps.setString(1,"1234");
+        ps.setString(2,"Sujin");
+        ps.setString(3,"1234");
 
         ps.executeUpdate();
         ps.close();
         conn.close();
     }
 
+    public void select(int id) throws SQLException {
+        ps = conn.prepareStatement("select * from users where id=?");
+        ps.setInt(1,id);
+
+        rs = ps.executeQuery();
+
+        while (rs.next()){
+            System.out.println("id: "+rs.getString(1));
+            System.out.println("name: "+rs.getString(2));
+            System.out.println("password: "+rs.getString(3));
+        }
+    }
+
     public static void main(String[] args) throws SQLException {
         SQLtest test = new SQLtest();
-        test.add();
+//        test.add();
+        test.connect();
+        test.select(1);
     }
 
 }
